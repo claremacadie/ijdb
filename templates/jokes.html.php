d98e22aa3a4b4d4e9d84a95373ed38da9758a042
Clare MacAdie <clare@macadie.co.uk>
2019-04-20 18:36:02 +0100

    Updated joke controller list method to pass a list of categories to the template. Updated jokes.html.php to create a list of a categories on the jokes list page. Updated jokes.css to highlight formatting for the categories list.

--- templates/jokes.html.php
+++ templates/jokes.html.php
@@ -1,41 +1,51 @@
 <?php //This file creates the html code for the jokes list page on the website?>
 
+<?php//Loop through the categories and create a list with links for each one?>
+<div class="jokelist">
+	<ul class="categories">
+		<?php foreach ($categories as $category): ?>
+			<li><a href="/joke/list?category=<?=$category->id?>"><?=$category->name?></a></li>
+		<?php endforeach; ?>
+	</ul>
+
 <?php //Outputs the total number of jokes?>
-<p><?=$totalJokes?> jokes have been submitted to the Internet Joke Database.</p>
+<div class="jokes">
+	<p><?=$totalJokes?> jokes have been submitted to the Internet Joke Database.</p>
 
-<?php //Outputs a list of jokes with an email link for the author, date (formatted to 1st april 2019), edit link and delete button?>
-<?php foreach ($jokes as $joke): ?>
-<blockquote>
-	<p>
-		<?=htmlspecialchars($joke->jokeText, ENT_QUOTES, 'UTF-8')?>
-		
-		(by 
-		<a href="mailto:<?php echo htmlspecialchars($joke->getAuthor()->email, ENT_QUOTES, 'UTF-8'); ?>">
-		<?=htmlspecialchars($joke->getAuthor()->name, ENT_QUOTES, 'UTF-8'); ?>
-		</a> 
-		
-		on 
-		
-		<?php
-		$date = new DateTime($joke->jokeDate);
-		echo $date->format('jS F Y');
-		?>)
-		
-		<?php//When a user is logged in, if their userId matches the authorId of a joke, the edit and delete actions are available?>
-		<?php//Otherwise, just the joke is listed?>	
-		<?php if ($userId == $joke->authorId): ?>
-			<a href ="/joke/edit?id=<?=$joke->id?>">
-			Edit
-			</a>
-		
-			<form action="/joke/delete" method="post">
-				<input type="hidden" name="id" value="<?=$joke->id?>">
-				<input type="submit" value="Delete">
-			</form>
-		
-		<?php endif; ?>
-		
-	</p>
-</blockquote>
-<?php endforeach; ?>
+	<?php //Outputs a list of jokes with an email link for the author, date (formatted to 1st april 2019), edit link and delete button?>
+	<?php foreach ($jokes as $joke): ?>
+	<blockquote>
+		<p>
+			<?=htmlspecialchars($joke->jokeText, ENT_QUOTES, 'UTF-8')?>
+			
+			(by 
+			<a href="mailto:<?php echo htmlspecialchars($joke->getAuthor()->email, ENT_QUOTES, 'UTF-8'); ?>">
+			<?=htmlspecialchars($joke->getAuthor()->name, ENT_QUOTES, 'UTF-8'); ?>
+			</a> 
+			
+			on 
+			
+			<?php
+			$date = new DateTime($joke->jokeDate);
+			echo $date->format('jS F Y');
+			?>)
+			
+			<?php//When a user is logged in, if their userId matches the authorId of a joke, the edit and delete actions are available?>
+			<?php//Otherwise, just the joke is listed?>	
+			<?php if ($userId == $joke->authorId): ?>
+				<a href ="/joke/edit?id=<?=$joke->id?>">
+				Edit
+				</a>
+			
+				<form action="/joke/delete" method="post">
+					<input type="hidden" name="id" value="<?=$joke->id?>">
+					<input type="submit" value="Delete">
+				</form>
+			
+			<?php endif; ?>
+			
+		</p>
+	</blockquote>
+	<?php endforeach; ?>
 
+</div>
